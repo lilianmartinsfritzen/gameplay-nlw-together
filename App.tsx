27ee8327cf691c22d6436f529146140226fff0d7
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-05-21 23:14:31 -0300

    feat: component sign in started

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { SignIn } from './src/screens/SignIn'
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+    <SignIn />
   )
 }

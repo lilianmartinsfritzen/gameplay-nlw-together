3c3087de745dc3f763a3a351fb7eee02b6eb9f5a
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-05-22 00:20:32 -0300

    fix: sign in screen adjustments

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { 
   View, 
   Text,
-  Image,
-  StatusBar
+  Image
 } from 'react-native'
 
 import { ButtonIcon } from '../../components/ButtonIcon'
@@ -14,11 +13,6 @@ import Illustration from '../../assets/illustration.png'
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <StatusBar 
-        barStyle='light-content'
-        backgroundColor='transparent'
-        translucent 
-      />
       <Image 
         source={Illustration} 
         style={styles.image}
@@ -27,13 +21,13 @@ export function SignIn() {
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Conecte-se{`\n`}
-          e organize suas{`\n`}
+          Conecte-se{'\n'}
+          e organize suas{'\n'}
           jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games{`\n`}
+          Crie grupos para jogar seus games{'\n'}
           favoritos com seus amigos
         </Text>
 

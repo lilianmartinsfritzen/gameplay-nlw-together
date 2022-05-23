c2b3a3527a93b16f441e72e2b62dc15691cd5043
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-05-22 22:43:45 -0300

    feat: avatar added to profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Avatar } from '../Avatar';
 
 import { styles } from './styles';
 
 export function Profile() {
   return (
     <View style={styles.container}>
+
+      <Avatar 
+        urlImage='https://github.com/lilianmartinsfritzen.png'
+      />
+
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>

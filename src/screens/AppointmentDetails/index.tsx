0880a7de3c2765f64c8d96950487aa2a52e4e91d
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-06-17 21:51:17 -0300

    feat: number of online members displayed

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { FlatList, ImageBackground, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Alert, FlatList, ImageBackground, Text, View } from 'react-native'
 import { useRoute } from '@react-navigation/native'
 import { BorderlessButton } from 'react-native-gesture-handler'
 
 import { Fontisto } from '@expo/vector-icons'
+
+import { api } from '../../services/api'
 
 import { AppointmentProps } from '../../components/Appointment'
 import { ListDivider } from '../../components/ListDivider'
@@ -11,84 +13,89 @@ import { Background } from '../../components/Background'
 import { ListHeader } from '../../components/ListHeader'
 import { ButtonIcon } from '../../components/ButtonIcon'
 import { Header } from '../../components/Header'
-import { Member } from '../../components/Member'
+import { Member, MemberProps } from '../../components/Member'
 
 import { styles } from './styles'
 import { theme } from '../../global/styles/theme'
 import BannerImg from '../../assets/banner.png'
+import { Load } from '../../components/Load'
 
 type Params = {
   guildSelected: AppointmentProps
 }
 
+type GuildWidget = {
+  id: string
+  name: string
+  instant_invite: string
+  members: MemberProps[]
+}
+
 export function AppointmentDetails() {
+  const [loading, setLoading] = useState(true)
+  const [widget, setWidget] = useState<GuildWidget>({} as GuildWidget)
+
   const route = useRoute()
   const { guildSelected } = route.params as Params
 
-  const members = [
-    {
-      id: '1',
-      username: 'Lílian',
-      avatar_url: 'https://github.com/lilianmartinsfritzen.png',
-      status: 'online'
-    },
-    {
-      id: '2',
-      username: 'LiliMF',
-      avatar_url: 'https://github.com/lilianmartinsfritzen.png',
-      status: 'offline'
+  async function fetchGuildWidget() {
+    try {
+      const response = await api.get(`/guilds/${guildSelected.guild.id}/widget.json`)
+      setWidget(response.data)
+
+    } catch {
+      Alert.alert(
+        'Verifique as configurações do servidor. Será que o Widget está habilitado?'
+      )
+    } finally {
+      setLoading(false)
     }
-  ]
+  }
+
+  useEffect(() => {
+    fetchGuildWidget()
+  },[])
 
   return (
     <Background>
-      <Header 
+      <Header
         title='Detalhes'
         action={
           <BorderlessButton>
-            <Fontisto 
-              name='share'
-              size={24}
-              color={theme.colors.primary}
-            />
+            <Fontisto name='share' size={24} color={theme.colors.primary} />
           </BorderlessButton>
         }
       />
 
-      <ImageBackground
-        source={BannerImg}
-        style={styles.banner}
-      >
+      <ImageBackground source={BannerImg} style={styles.banner}>
         <View style={styles.bannerContent}>
-          <Text style={styles.title}>
-            { guildSelected.guild.name }
-          </Text>
+          <Text style={styles.title}>{guildSelected.guild.name}</Text>
 
-          <Text style={styles.subtitle}>
-            { guildSelected.description }
-          </Text>
+          <Text style={styles.subtitle}>{guildSelected.description}</Text>
         </View>
       </ImageBackground>
 
-      <ListHeader 
-        title='Jogadores'
-        subtitle='Total 3'
-      />
+      {loading ? (
+        <Load />
+      ) : (
+        <>
+          <ListHeader 
+            title='Jogadores' 
+            subtitle={`Total ${widget.members.length}`}
+          />
 
-      <FlatList 
-        data={members}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <Member data={item} />
-        )}
-        ItemSeparatorComponent={() => <ListDivider isCentered />}
-        style={styles.members}
-      />
+          <FlatList
+            data={widget.members}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <Member data={item} />}
+            ItemSeparatorComponent={() => <ListDivider isCentered />}
+            style={styles.members}
+          />
+        </>
+      )}
 
       <View style={styles.footer}>
-        <ButtonIcon 
-          title='Entrar na partida'
-        />
+        <ButtonIcon title='Entrar na partida' />
       </View>
     </Background>
   );
